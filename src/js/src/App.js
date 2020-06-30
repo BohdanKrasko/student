9b23e1befdcd34a23099b60bb1dc778bb01ac554
Bohdan <krasko200006@gmail.com>
2020-06-30 11:57:59 +0300

    adding react state

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import { getAllStudents } from './client';
 
-function App() {
-	getAllStudents().then(res => res.json().then(students => {
-		console.log(students);
-	}));
-	return <h1>Students</h1>
+
+class App extends Component {
+	state = {
+		students: []
+	}
+	componentDidMount () {
+		this.fetchStudents();
+	}
+	fetchStudents = () => {
+		getAllStudents()
+		.then(res => res.json()
+		.then(students => {
+			console.log(students)
+			this.setState({students})
+		}));
+	}
+	
+	render() {
+		const {students} = this.state;
+		if(students && students.length) {
+
+			return students.map((student, index) => {
+				return (
+				<div key = {index}>
+					<h1>index = {index}</h1>
+				<h2>{student.studentId}</h2>
+				<p>{student.firstName}</p>
+				<p>{student.lastName}</p>
+				<p>{student.email}</p>
+				<p>{student.gender}</p>
+				</div>
+				);
+			})
+		}
+		return <h1>No Students found</h1>;
+	}
+	
 }
 
 export default App;
